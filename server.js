a4e2e64dc3d73baf5837392737706dac9664a5a1
nkeze33 <57569103+nkeze33@users.noreply.github.com>
2026-03-24 18:54:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.send('Borderless Crafts backend is running.'));
+  res.send('Borderless Crafts backend is running.');
 });
 
 app.get('/products', (req, res) => {
